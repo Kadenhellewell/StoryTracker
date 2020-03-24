b8fc3a9479164946d39529b4297f9aca59e74277
Kaden <kdex95@yahoo.com>
2020-03-24 16:09:45 -0600

    I'm trying to figure out file stuff between Story, SubStory, and Save

--- src/Story.java
+++ src/Story.java
@@ -6,13 +6,15 @@ import java.util.ArrayList;
 
 public class Story
 {
-    String Name;
+    String name;
     File directory; //call mkdir() in the constructor. This directory should only have subStories
     ArrayList<SubStory> subStories = new ArrayList<>();
+    String currentDir = "";
+
 
     Story(String name)
     {
-        String currentDir = System.getProperty("user.dir");
+        currentDir = System.getProperty("user.dir");
         Path newPath = Paths.get(currentDir, name);
         directory = newPath.toFile();
         if(!directory.exists())
@@ -34,23 +36,21 @@ public class Story
 
     public void addSub(SubStory subStory)
     {
+        // Go into the substory directory to add the new file there
         subStories.add(subStory);
-        try
+        Path proposed = Paths.get(currentDir, directory.toString());
+        if(proposed.toFile().isDirectory())
         {
-            File myObj = new File("filename.txt");
-            if (myObj.createNewFile())
-            {
-                System.out.println("File created: " + myObj.getName());
-            }
-            else
-            {
-                System.out.println("File already exists.");
-            }
+            System.setProperty("user.dir", proposed.toString());
         }
-        catch (IOException e)
+        else
         {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            System.out.print("Unable to go into this story's substory directory\n");
         }
+
+
+        // Go back out so that other stories can be accessed
+        File fileCurrent = new File(System.getProperty("user.dir"));
+        System.setProperty("user.dir", fileCurrent.getParent());
     }
 }
--- src/SubStory.java
+++ src/SubStory.java
@@ -1,4 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class SubStory extends Story
 {
+    File subStory;
+    Story parent;
+    ArrayList<SubStory> saves = new ArrayList<>();
+    SubStory(String name)
+    {
+        super(name);
+        try
+        {
+            subStory = new File(name + ".txt");
+            if (subStory.createNewFile())
+            {
+                System.out.println("File created: " + subStory.getName());
+            }
+            else
+            {
+                System.out.println("File already exists.");
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 
+    public void save()
+    {
+
+    }
 }
